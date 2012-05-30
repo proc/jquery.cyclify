ac5516a425a008ca3d83c275236191a5ff25f760
Peter Tran <ptran@optimiscorp.com>
2012-05-30 15:45:50 -0700

    Refactoring

--- jquery.cyclify.js
+++ jquery.cyclify.js
@@ -5,94 +5,106 @@
 (function($) {
   $.fn.extend({
     cyclify : function(options) {
-      var defaults = {
-        loop : false,
-        transition: 'swing',
-        delay : 0,
-        begin_at : 0
-      }
-      var $controls     = this.find('.cycle-controls'),
-          $begin        = $controls.find('.cycle-begin'),
-          $prev         = $controls.find('.cycle-prev'),
-          $next         = $controls.find('.cycle-next'),
-          $end          = $controls.find('.cycle-end'),
-          $cycles       = this.find('.cycle-cycles div'),
-          max           = $cycles.length,
-          current_index = 0,
-          options       = $.extend(defaults, options);
-      
-      function _set_current_cycle(num) {
-        current_index = num;
-        $cycles.removeClass('current-cycle');
-        $cycles.hide();
-        $($cycles[num]).show(options.delay, options.transition).addClass('current-cycle');
 
+      var Cycler = function($element, options) {
+        this.container = $element;
+        this.settings = {
+          loop       : false,
+          transition : 'swing',
+          delay      : 0,
+          begin_at   : 0,
+          next       : '.cycle-next',
+          previous   : '.cycle-prev',
+          begin      : '.cycle-begin',
+          end        : '.cycle-end',
+          cycles     : '.cycle-cycles div',
+          controls   : '.cycle-controls',
+          success    : null
+        };
+        $.extend(this.settings, options);
+
+        this.elements = {};
+        this.elements.controls = this.container.find(this.settings.controls);
+        this.elements.cycles = this.container.find(this.settings.cycles);
+        this.elements.max = this.elements.cycles.length;
+        this.elements.current_index = 0;
+
+        $.extend(this.elements, {
+          begin         : this.elements.controls.find(this.settings.begin),
+          prev          : this.elements.controls.find(this.settings.previous),
+          next          : this.elements.controls.find(this.settings.next),
+          end           : this.elements.controls.find(this.settings.end)
+        });
+        this._init();
       }
-      
-      function _callback_if_necessary() {
-        if(typeof options.success === 'function') {
-          options.success($($cycles[current_index]));
+
+      Cycler.prototype._init = function() {
+        var $this = this;
+        this.elements.next.die().live('click', function() {
+          $this.next();
+        });
+        this.elements.prev.die().live('click', function() {
+          $this.prev();
+        });
+        this.elements.begin.die().live('click', function() {
+          $this.begin();
+        });
+        this.elements.end.die().live('click', function() {
+          $this.end();
+        });
+        this._set_current_cycle(this.settings.begin_at);
+        this.callback_if_necessary();
+      };
+
+      Cycler.prototype._set_current_cycle = function(num) {
+        this.elements.current_index = num;
+        this.elements.cycles.removeClass('current-cycle');
+        this.elements.cycles.hide();
+        $(this.elements.cycles[num]).show(this.settings.delay, this.settings.transition).addClass('current-cycle');
+      }
+
+      Cycler.prototype.callback_if_necessary = function() {
+        if(typeof this.settings.success === 'function') {
+          this.settings.success($(this.elements.cycles[this.elements.current_index]));
         }
       }
 
-      var next = function next() {
-        var cycle_num = current_index,
-            $current = $($cycles[cycle_num]);
-        if(cycle_num < (max - 1)) { 
-          _set_current_cycle(cycle_num + 1);
-          callback_if_necessary();
+      Cycler.prototype.next = function() {
+        var cycle_num = this.elements.current_index;
+        if(cycle_num < (this.elements.max - 1)) {
+          this._set_current_cycle(cycle_num + 1);
+          this.callback_if_necessary();
         } else {
-          if(options.loop) {
-            begin();
+          if(this.settings.loop) {
+            this.begin();
           }
         }
       };
-      var prev = function prev() {
-        var cycle_num = current_index,
-        $current = $($cycles[cycle_num]);
-        if(cycle_num > 0) { 
-          _set_current_cycle(cycle_num - 1);
-          callback_if_necessary();
+
+      Cycler.prototype.prev = function() {
+        var cycle_num = this.elements.current_index;
+        if(cycle_num > 0) {
+          this._set_current_cycle(cycle_num - 1);
+          this.callback_if_necessary();
         } else {
-          if(options.loop) {
-            end();
+          if(this.settings.loop) {
+            this.end();
           }
         }
       };
-      var begin = function begin() {
-        _set_current_cycle(0);
-        callback_if_necessary();
-      };
-      var end = function end() {
-        _set_current_cycle(max - 1);
-        callback_if_necessary();
+
+      Cycler.prototype.begin = function() {
+        this._set_current_cycle(0);
+        this.callback_if_necessary();
       };
 
-      var _init = function() {
-        $next.die().live('click', function() {
-          next();
-        });
-        $prev.die().live('click', function() {
-          prev();
-        });
-        $begin.die().live('click', function() {
-          begin();
-        });
-        $end.die().live('click', function() {
-          end();
-        });
-        _set_current_cycle(options.begin_at);
-        callback_if_necessary();
-      }
+      Cycler.prototype.end = function() {
+        this._set_current_cycle(this.elements.max - 1);
+        this.callback_if_necessary();
+      };
 
-      _init();
-
-      return {
-        next  : next,
-        prev  : prev,
-        begin : begin,
-        end   : end,
-      }
+      return new Cycler(this, options);
     }
 });
 })(jQuery);
+
